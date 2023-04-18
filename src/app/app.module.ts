7dbee4648f8f5dc293ba8e4d1a06dc052557bb96
Giuliana Molaro <123484973+giumolaro159@users.noreply.github.com>
2023-04-17 22:14:36 -0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeMiComponent } from './components/acerca-de-mi/acerca-de-mi.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeMiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent
   ],
   imports: [
+    NgCircleProgressModule.forRoot({}),
     BrowserModule,
     AppRoutingModule
   ],
